d46754837ae8e7907fa666533922afef00a3ad72
navneesh <navneesh@gmail.com>
2025-07-10 15:24:35 +0530

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,12 +8,19 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     public float jumpForce = 5f;
     private bool isGrounded = true; // Simple grounded check for demo
+    private BoxCollider2D boxCollider;
+    private Vector2 originalColliderSize;
+    private Vector2 originalColliderOffset;
+    public float crouchHeightMultiplier = 0.5f;
     
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        originalColliderSize = boxCollider.size;
+        originalColliderOffset = boxCollider.offset;
     }
 
     // Update is called once per frame
@@ -50,11 +57,16 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
         {
             animator.SetBool("Crouch", true);
-            // Optionally, adjust collider size here
+            // Adjust collider size for crouch
+            boxCollider.size = new Vector2(originalColliderSize.x, originalColliderSize.y * crouchHeightMultiplier);
+            boxCollider.offset = new Vector2(originalColliderOffset.x, originalColliderOffset.y - (originalColliderSize.y * (1 - crouchHeightMultiplier) / 2f));
         }
         else
         {
             animator.SetBool("Crouch", false);
+            // Restore collider size
+            boxCollider.size = originalColliderSize;
+            boxCollider.offset = originalColliderOffset;
         }
     }
 
